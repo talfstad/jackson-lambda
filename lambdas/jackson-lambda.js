50ba32b6ae85c8bbb05103edbee9baccd553cd4a
Trevor Alfstad <trevor@buildcave.com>
2017-05-29 11:02:35 -0700

    feat: example proxy function. https://app.asana.com/0/339740516165222/351302234957098

--- lambdas/jackson-lambda.js
+++ lambdas/jackson-lambda.js
@@ -1,28 +1,18 @@
-const AWS = require('aws-sdk');
-
 exports.handler = (event, context, callback) => {
-  AWS.config.loadFromPath('./config/aws.json');
+  const name = 'World';
+  const responseCode = 200;
 
-  const { alias } = event.params;
-
-  console.log(alias);
-  console.log('^ alias');
-  const lambda = new AWS.Lambda();
-
-  const jacksonCoreConfig = {
-    FunctionName: 'jackson-core',
-    Payload: JSON.stringify({}),
-    InvocationType: 'Event',
-    Qualifier: alias,
+  const responseBody = {
+    message: `Hello ${name}!`,
+    input: event,
   };
-
-  lambda.invoke(jacksonCoreConfig, (err, data) => {
-    console.log(err);
-    console.log(data);
-    if (err) {
-      callback(err);
-    } else {
-      callback(false, data);
-    }
-  });
+  const response = {
+    statusCode: responseCode,
+    headers: {
+      'x-custom-header': 'my custom header value',
+    },
+    body: JSON.stringify(responseBody),
+  };
+  console.log(`response: ${JSON.stringify(response)}`);
+  callback(null, response);
 };
--- test/lambdas/jackson-lambda.js
+++ test/lambdas/jackson-lambda.js
@@ -4,7 +4,7 @@ import lambda from '../../lambdas/jackson-lambda';
 describe('lambdas', () => {
   describe('jackson-lambda', () => {
     it('can run jackson-lambda and mock inputs', () => {
-      lambda.handler({ params: { alias: 'test' } }, context, (err, result) => {
+      lambda.handler({}, context, (err, result) => {
         expect(result).to.equal(result);
       });
     });
