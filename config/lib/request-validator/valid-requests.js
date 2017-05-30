2960f9298b5bcb1bd24a37344e64b06105a5c34b
Trevor Alfstad <trevor@buildcave.com>
2017-05-30 08:40:27 -0700

    feat: built lib for updated validation request. https://app.asana.com/0/351262640945077/351714261806361

--- config/lib/request-validator/valid-requests.js
+++ config/lib/request-validator/valid-requests.js
@@ -1,4 +1,12 @@
 module.exports = {
+  'test.cdnjs.io': {
+    '/ajax/libs/jquery/3.2.1/jquery.min.js': ['GET'],
+    '/ajax/libs/jquery/3.2.0/jquery.min.js': ['GET'],
+    '/ajax/libs/jquery/3.1.1/jquery.min.js': ['GET'],
+    '/ajax/libs/jquery/3.1.0/jquery.min.js': ['GET'],
+    '/ajax/libs/jquery/3.0.0/jquery.min.js': ['GET'],
+    '/ajax/libs/jquery/2.2.4/jquery.min.js': ['GET'],
+  },
   'cloudflare.cdnjs.io': {
     '/ajax/libs/jquery/3.2.1/jquery.min.js': ['GET'],
     '/ajax/libs/jquery/3.2.0/jquery.min.js': ['GET'],
