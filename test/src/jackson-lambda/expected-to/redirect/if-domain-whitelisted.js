164c818ecfaadc59e5a09f4b486ffef914b07332
Trevor Alfstad <trevor@buildcave.com>
2017-06-14 18:52:27 -0700

    feat: if-domain-whitelisted test complete, setup tear down db using redis and mongo dao. https://app.asana.com/0/362270740962664/364081115616322

--- test/src/jackson-lambda/expected-to/redirect/if-domain-whitelisted.js
+++ test/src/jackson-lambda/expected-to/redirect/if-domain-whitelisted.js
@@ -1,32 +1,72 @@
 import {
     expect,
 } from 'chai';
-import lambda from '../../../../../src';
 
-const event = {
-  path: '/jquery/dist',
-  httpMethod: 'GET',
-  headers: {
-    Host: 'github-cdn.com',
-    'X-Forwarded-Proto': 'https',
-    'X-Forwarded-For': '2602:304:ce3e:27f0:1e:abc8:9568:8b1, 205.251.214.90',
-    'X-Alt-Referer': 'http://yourtrendingnews.com/lebron/?voluumdata=BASE64dmlkLi4wMDAwMDAwMi05N2UwLTQ1NDctODAwMC0wMDAwMDAwMDAwMDBfX3ZwaWQuLjZhOGIyODAwLTQ1NTktMTFlNy04MjJjLTdiNmFjYmQ1YmZiNV9fY2FpZC4uYmZkNjA5MTAtMzlmYS00M2U2LThjZmItN2Y0MGFhMmFhOGJiX19ydC4uUl9fbGlkLi4zYTNhMWRjNy1lZTc5LTRjMWItYTI3Ni1iN2VhMDViZGIzZjBfX29pZDEuLmI4MWFjYWNkLWI3Y2UtNDUxOC1hN2E3LTg0MjI3OTcxNGY5ZF9fdmFyMS4ubW9iaWxlcG9zc2UtaG90bGlua3NfX3ZhcjIuLkpvcmRhbiBDYWxscyBPdXQgTGVicm9uX192YXIzLi5odHRwOi8vbGwtbWVkaWFcLlx0bXpcLlxjb20vMjAxMy8xMC8wMS8xMDAxLWpvcmRhbi1sZWJyb24ta29iZS0zXC5canBnX19yZC4uc3BvcnRzdXBkYXRlcnNcLlxjb21fX2FpZC4uX19hYi4uX19zaWQuLl9fY3JpLi5fX3B1Yi4uX19kaWQuLl9fZGl0Li5fX3BpZC4uX19pdC4uX192dC4uMTQ5NjE2NDg1MjQzOA&site=mobileposse-hotlinks&title=Jordan%20Calls%20Out%20Lebron&thumbnail=http://ll-media.tmz.com/2013/10/01/1001-jordan-lebron-kobe-3.jpg?txid=994c3823-aff6-f548-ce9b-1b5df2ac267c',
-    Referer: 'https://some-lander.com/landingpage.html',
-  },
-  stageVariables: {
-    redirectHost: 'github.com',
-    alias: 'test',
-  },
-};
+import MongoDao from '../../../../../lib/jackson-core/lib/dao/mongo-dao';
+import RedisDao from '../../../../../lib/jackson-core/lib/dao/redis-dao';
+import Config from '../../../../../lib/jackson-core/config';
+
+import lambda from '../../../../../src';
 
 describe('Jackson Lambda', () => {
   describe('Expected to', () => {
-    it('Redirect if domain is whitelisted', (done) => {
-      // add domain to whitelisted collection in mongo
-      // test redirect
-      // remove domain from whitelisted collection
+    const event = {
+      path: '/jquery/dist',
+      httpMethod: 'GET',
+      headers: {
+        Host: 'github-cdn.com',
+        'X-Forwarded-Proto': 'https',
+        'X-Forwarded-For': '2602:304:ce3e:27f0:1e:abc8:9568:8b1, 205.251.214.90',
+        'X-Alt-Referer': 'http://yourtrendingnews.com/lebron/?txid=994c3823-aff6-f548-ce9b-1b5df2ac267c',
+        Referer: 'test-whitelisted-domain.com',
+      },
+      stageVariables: {
+        redirectHost: 'github.com',
+        alias: 'test',
+      },
+    };
 
-      lambda.handler({ ...event, headers: { ...event.headers, Referer: 'http://test-whitelisted-domain.com' } }, {}, (err, response) => {
+    let mongoDao = null;
+    let redisDao = null;
+
+    before((done) => {
+      redisDao = new RedisDao({
+        config: new Config({ stageVariables: event.stageVariables }).redisDaoConfig(),
+      });
+      mongoDao = new MongoDao({
+        config: new Config({ stageVariables: event.stageVariables }).mongoDaoConfig(),
+      });
+
+      // clear out redis whitelisted_domains to start fresh
+      redisDao.delWhitelistedDomains()
+        // add domain to whitelisted collection in mongo
+        .then(() => mongoDao.whitelistDomain(event.headers.Referer))
+        .then(() => {
+          redisDao.closeConnection();
+          mongoDao.closeConnection();
+          done();
+        });
+    });
+
+    after((done) => {
+      // clean up whitelistedDomains in redis from test
+      redisDao.delWhitelistedDomains()
+        // clean up test domain from whitelisted_domains collection
+        .then(() => mongoDao.removeWhitelistedDomain(event.headers.Referer))
+        .then(() => {
+          redisDao.closeConnection();
+          mongoDao.closeConnection();
+          done();
+        });
+    });
+
+    it.only('Redirect if domain is whitelisted', (done) => {
+      // To test if domain is whitelisted we need to get far along
+      // enough to get into the request validator. So, we need to make sure
+      // We offer valid inputs but we don't need to offer more input than
+      // that since we should never get to the decision engine
+      // or need more information.
+      lambda.handler(event, {}, (err, response) => {
         try {
           expect(err).to.equal(null);
           const {
