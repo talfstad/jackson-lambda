4dc076639088588be00df3d7205c84483955b835
Trevor Alfstad <trevor@buildcave.com>
2017-06-24 12:23:43 -0700

    feat: request finalization input test. throw error on class instantiations with no dao. https://app.asana.com/0/0/372455338229813

---
+++ test/lib/jackson-core/request-finalization/finalize-request/expected-to/fail/given-incorrect-inputs.js
@@ -0,0 +1,59 @@
+import _ from 'lodash';
+import RequestFinalization from '../../../../../../../lib/jackson-core/lib/request-finalization';
+import Config from '../../../../../../../lib/jackson-core/config';
+import Dao from '../../../../../../../lib/jackson-core/lib/dao';
+
+describe('Jackson Lambda', () => {
+  describe('Jackson Core', () => {
+    describe('RequestFinalization', () => {
+      describe('finalizeRequest', () => {
+        describe('Expected to', () => {
+          const config = Config({ stageVariables: {} });
+
+          const decisionInformation = {
+            userConfig: {
+              last_updated: new Date(),
+              min_minutes_consecutive_traffic: 3,
+              min_daily_hits_to_take: 4,
+              min_hits_per_min_to_take: 10,
+            },
+            requestInputs: {
+              uuid: '994c3823-aff6-f548-ce9b-1b5df2ac267c',
+              ip: '2602:304:ce3e:27f0:1e:abc8:9568:8b1',
+              url: 'https://some-lander.com/landingpage.html',
+              geo: { country: 'US' },
+            },
+            updatedRipRecord: {
+
+            },
+          };
+
+          const jackDecision = false;
+
+          const validInputs = {
+            decisionInformation,
+            jackDecision,
+          };
+
+          it('Fail if not given inputs object with jackDecision key', (done) => {
+            const db = new Dao({ config });
+            new RequestFinalization({ db }).finalizeRequest(_.omit(validInputs, 'jackDecision'))
+              .then(() => {
+                done(new Error('Failed to recognize incorrect inputs'));
+              })
+              .catch(() => done());
+          });
+
+          it('Fail if not given inputs object with decisionInformation key', (done) => {
+            const db = new Dao({ config });
+            new RequestFinalization({ db }).finalizeRequest(_.omit(validInputs, 'decisionInformation'))
+              .then(() => {
+                done(new Error('Failed to recognize incorrect inputs'));
+              })
+              .catch(() => done());
+          });
+        });
+      });
+    });
+  });
+});
